3bea186c36010050d40c1be67f51a2ba0bd054a1
degenwitz <dimitri.degkwitz@uzh.ch>
2024-01-31 22:18:44 +0100

    bevors weg geht

--- dog.py
+++ dog.py
@@ -2,7 +2,7 @@ from board import (FigureButton, BoardGraphic)
 from boardDTO import Board
 from PyQt5.QtWidgets import QApplication
 from playerClass import PlayerClass
-from randomPlayer import RandomPlayerBot
+from randomPlayer import RandomPlayerBot, get_playable_cards
 from Cards import Card, EasyCard, Four, Seven, King, Ass, Joker
 import random
 from global_vals import spielfeld, zuhause, ziele
@@ -51,16 +51,14 @@ class GameRunner():
                 self.players[(self.currentPlayer+j)%4].addCard(self.deck[0])
                 self.boardGraphic.addCard(self.deck[0],("player", j))
                 self.deck = self.deck[1:]
-
-        #for j in range(4):
-            #self.players[(self.currentPlayer + j) % 4].addCard(Ass("♣")) ##TODO: Remove this, only for testing
+        self.__still_need_to_swap_cards = True
 
         self.currentHandSize -= 1
         if self.currentHandSize < 2:
             self.currentHandSize = 5
 
     def executeCard(self, playerNumber, card):
-        if isinstance(card, King):
+        if isinstance(card, King) and card.get_exit() == True:
             for figs in self.boardGraphic.getFigures():
                 if figs.get_spielernummer() == playerNumber:
                     if figs.get_aktuelles_feld()[0] in zuhause:
@@ -70,19 +68,54 @@ class GameRunner():
             figure = self.boardGraphic.getFigure(card.get_target_figure(),playerNumber)
             figure.ziehen(card.get_value(), card.get_enter_if_possible() )
 
+    def swap_command_for_player(self, i:int):
+            cards = self.players[i].getHand()
+            board, figuren, game = self.generatePlayerObjects(i)
+            swapedCard = self.playersBots[i].swapCard(cards, board, figuren, game)
+            return swapedCard
+
     def nextMove(self):
-        cards = self.players[self.currentPlayer].getHand()
-        if len(cards) != 0:
-            print(self.currentPlayer)
-            board, figuren, game = self.generatePlayerObjects(self.currentPlayer)
-            playedCard = self.playersBots[self.currentPlayer].playCard(cards, board, figuren, game)
-            self.executeCard(self.currentPlayer, playedCard)
-            self.players[self.currentPlayer].removeCard(playedCard)
-            self.discard += (playedCard,)
-        self.currentPlayer = (self.currentPlayer + 1)%4
+        if( self.__still_need_to_swap_cards):
+            cards = {}
+            for player in range(4):
+                cards[ (player, (player+2)%4) ] = self.swap_command_for_player(player)
+            for partners, card in cards.items():
+                self.players[partners[0]].removeCard(card)
+                self.players[partners[1]].addCard(card)
+                self.boardGraphic.addCard(card,("player", partners[1]))
+            self.__still_need_to_swap_cards = False
+        else:
+            cards = self.players[self.currentPlayer].getHand()
+            if len(cards) != 0:
+                board, figuren, game = self.generatePlayerObjects(self.currentPlayer)
+                if len(get_playable_cards(cards, board, figuren)) == 0:
+                    for card in cards:
+                        self.players[self.currentPlayer].removeCard(card)
+                        self.discard += (card,)
+                        self.boardGraphic.addCard(card, ("discard",) )
+                else:
+                    playedCard = self.playersBots[self.currentPlayer].playCard(cards, board, figuren, game)
+                    self.executeCard(self.currentPlayer, playedCard)
+                    self.players[self.currentPlayer].removeCard(playedCard)
+                    self.discard += (playedCard,)
+                    self.boardGraphic.addCard(playedCard, ("discard",) )
+            self.__increase_turn()
+
+
+    def __increase_turn(self):
+        self.set_turn((self.currentPlayer + 1)%4)
+
+    def set_turn(self, i:int):
+        self.currentPlayer = i
+        self.boardGraphic.set_current_player(self.currentPlayer)
+
 
     def setGraphicBoard(self, boardGraphic):
         self.boardGraphic = boardGraphic
+        i = 0
+        for player in self.players:
+            player.set_graphic( boardGraphic.get_player_graphic(i) )
+            i+=1
 
     def generatePlayerObjects(self, playerNumber):
         figuresGr = self.boardGraphic.getFigures()
@@ -106,13 +139,58 @@ class GameRunner():
         figuren = tuple(figuren)
         return board, figuren, game
 
+    def get_all_cards(self):
+        return self.deck + self.discard + self.players[0].getHand()+ self.players[1].getHand()+ self.players[2].getHand()+ self.players[3].getHand()
+
+    def init_game(self):
+        self.distributeCards()
+        self.boardGraphic.set_pile(self.deck, ("deck",) )
+        self.set_turn(0)
+
+    def skipTurn(self):
+        self.__increase_turn()
+
+    def __remove_old_owner_of_card(self, card:Card):
+        for player in self.players:
+            if card in player.getHand():
+                player.removeCard(card, allertGraphic=False)
+        if card in self.deck:
+            deck = list(self.deck)
+            deck.remove(card)
+            self.deck = tuple(deck)
+        if card in self.discard:
+            discard = list(self.discard)
+            discard.remove(card)
+            self.discard = tuple(discard)
+
+    def set_player_card(self, player_number:int, card:Card):
+        self.__remove_old_owner_of_card(card)
+        self.players[player_number].addCard(card, allertGraphic=False)
+
+    def set_deck_card(self, card:Card, i):
+        self.__remove_old_owner_of_card(card)
+        deck = list(self.deck)
+        if i < len(deck):
+            deck.insert(i, card)
+        else:
+            deck.append(card)
+        self.deck = tuple(deck)
+
+    def set_discard_card(self, card:Card, i):
+        self.__remove_old_owner_of_card(card)
+        discard = list(self.discard)
+        if i < len(discard):
+            discard.insert(i, card)
+        else:
+            discard.append(card)
+        self.discard = tuple(discard)
 
 
 game = GameRunner(RandomPlayerBot(),RandomPlayerBot(),RandomPlayerBot(),RandomPlayerBot())
 app = QApplication([])
 w = BoardGraphic(game)
 game.setGraphicBoard(w)
-game.distributeCards()
+game.init_game()
 w.show()
 
 app.exec_()
--- playerClass.py
+++ playerClass.py
@@ -1,8 +1,10 @@
+from board import PlayerGraphic
 
 
 class PlayerClass():
     def __init__(self):
         self.hand = ()
+        self.player_graphic = None
 
     def getHand(self):
         return  self.hand
@@ -10,13 +12,21 @@ class PlayerClass():
     def clearHand(self):
         self.hand = ()
 
-    def removeCard(self, card):
+    def set_graphic(self, playerGraphic: PlayerGraphic):
+        self.player_graphic = playerGraphic
+
+    def removeCard(self, card, allertGraphic=True):
         handList = list(self.hand)
         handList.remove(card)
         self.hand = tuple(handList)
+        if allertGraphic and self.player_graphic != None:
+            self.player_graphic.remove_card(card)
 
-    def addCard(self, card):
+    def addCard(self, card, allertGraphic=True):
         self.hand = self.hand + (card,)
+        if allertGraphic and self.player_graphic != None:
+            #self.player_graphic.add_card(card)
+            pass
 
     def newHand(self, hand):
         self.hand = hand
